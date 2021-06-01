a7aafc1ce46342d68492cc2f5966bae7729ba6a3
Гуров Георгий <magnezia98@mail.ru>
2021-06-01 17:02:57 +0300

    Token updated

--- source/js/variables.js
+++ source/js/variables.js
@@ -2,7 +2,7 @@ export const mainApiPath = 'https://api.twitch.tv/helix/';
 
 export const authorizObj = {
   headers: {
-    'Authorization': 'Bearer w0n85vsuhq4813hpq6wz31whbidq6t',
+    'Authorization': 'Bearer xo4umtvsfoarhdhhtu0par6ppis3md',
     'Client-Id': 'd1trdi4di1nxgpxk6h48qkg2tx0msc',
   },
 };
